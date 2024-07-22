744b3f07bcebea00abd55255ccb4209f305ec37a
Agos95 <agostini.federico.95@gmail.com>
2024-07-22 16:53:25 +0200

    update view starter dev

--- scripts/view-starter-dev.sh
+++ scripts/view-starter-dev.sh
@@ -11,20 +11,29 @@ trap "exitfn" INT            # Set up SIGINT trap to call function.
 # export HUGO_STATS_PATH="./starters/$1/hugo_stats.json"
 # printf 'HUGO_STATS_PATH: %s\n' "$HUGO_STATS_PATH"
 
+# create modules replace from contents in "./modules"
+modules_dir="$(pwd)/modules"
+modules="$(ls "$modules_dir")"
+hugo_module_replacements=""
+for module in $modules; do
+    module_github_prefix="github.com/HugoBlox/hugo-blox-builder/modules"
+    if [[ "${module}" == "${blox-bootstrap}" ]]; then
+        module_github_prefix="$module_github_prefix/v5"
+    fi
+    hugo_module_replacements="$module_github_prefix/$module -> $modules_dir/$module,$hugo_module_replacements"
+done
+# remove trailing comma
+hugo_module_replacements=${hugo_module_replacements::-1}
+
 # `--source "starters/$1"` won't work for Tailwind Module
 # due to Hugo limitation requiring Hugo to be run from site dir
-cd "starters/$1"
+starter_dir="starters/$1"
+cd "$starter_dir" || exit 1
 
-export WC_DEMO=true \
-export HUGO_ENVIRONMENT=development \
-export HUGOxPARAMSxDECAP_CMSxLOCAL_BACKEND=true \
-export HUGO_MODULE_REPLACEMENTS="github.com/HugoBlox/hugo-blox-builder/modules/blox-bootstrap/v5 -> ../../../modules/blox-bootstrap,
-github.com/HugoBlox/hugo-blox-builder/modules/blox-plugin-netlify -> ../../../modules/blox-plugin-netlify,
-github.com/HugoBlox/hugo-blox-builder/modules/blox-plugin-reveal -> ../../../modules/blox-plugin-reveal,
-github.com/HugoBlox/hugo-blox-builder/modules/blox-tailwind -> ../../../modules/blox-tailwind,
-github.com/HugoBlox/hugo-blox-builder/modules/blox-plugin-decap-cms -> ../../../modules/blox-plugin-decap-cms,
-github.com/HugoBlox/hugo-blox-builder/modules/blox-core -> ../../../modules/blox-core,
-github.com/HugoBlox/hugo-blox-builder/modules/blox-seo -> ../../../modules/blox-seo" \
+WC_DEMO=true \
+HUGO_ENVIRONMENT=development \
+HUGOxPARAMSxDECAP_CMSxLOCAL_BACKEND=true \
+HUGO_MODULE_REPLACEMENTS="$hugo_module_replacements" \
 hugo && \
 npm_config_yes=true npx pagefind --site "public" --output-subdir ../static/pagefind && \
 hugo server --panicOnWarning --renderStaticToDisk -F --port 8081 --bind 0.0.0.0
